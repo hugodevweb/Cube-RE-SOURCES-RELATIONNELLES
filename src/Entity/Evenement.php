d45259a647a46145a692a7da5daee2454468a1e2
ghostewer <lbouclaud@gmail.com>
2023-12-19 18:26:48 +0100

    Admin - Évènements : Gestion

--- src/Entity/Evenement.php
+++ src/Entity/Evenement.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use App\Repository\EvenementRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Monolog\DateTimeImmutable;
+use DateTimeZone;
+
 
 #[ORM\Entity(repositoryClass: EvenementRepository::class)]
 class Evenement
@@ -31,6 +34,11 @@ class Evenement
 
     #[ORM\Column]
     private ?\DateTimeImmutable $created_at = null;
+
+    public function __construct()
+    {
+        $this->created_at = new DateTimeImmutable(false, new DateTimeZone('Europe/Paris'));
+    }
 
     public function getId(): ?int
     {
