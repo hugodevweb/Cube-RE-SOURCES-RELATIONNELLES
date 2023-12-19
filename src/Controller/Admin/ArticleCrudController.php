bd51e9235d6f3016ea0eaadb59b99c15c0fb3e51
ghostewer <lbouclaud@gmail.com>
2023-12-19 15:34:50 +0100

    Admin - Article : Gestion

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,20 +3,31 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+
+use App\Repository\UtilisateurRepository;
+
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 
 class ArticleCrudController extends AbstractCrudController
 {
+    private $utilisateurRepository;
+
+    public function __construct(UtilisateurRepository $utilisateurRepository)
+    {
+        $this->utilisateurRepository = $utilisateurRepository;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Article::class;
     }
-
     
     public function configureFields(string $pageName): iterable
     {
@@ -24,9 +35,28 @@ class ArticleCrudController extends AbstractCrudController
             TextField::new('titre'),
             TextEditorField::new('corps'),
             Field::new('nombreVu')->onlyOnIndex(),
-            BooleanField::new('validation')
-                ->renderAsSwitch(false)
-                ->onlyOnIndex(),
+            BooleanField::new('validation')->onlyOnIndex(),
+            AssociationField::new('categories')->onlyOnForms(),
+            ChoiceField::new('utilisateur')->setChoices($this->formatUtilisateurForChoices($this->utilisateurRepository->findall()))->onlyOnForms(),
+            AssociationField::new('utilisateur')
+            ->setFormTypeOptions([
+                'by_reference' => false,
+            ])
+            ->autocomplete()
+            ->setCustomOptions([
+                'widget' => 'native',
+            ])
+            ->setRequired(true)
+            ->onlyOnIndex(),
         ];
+    }
+
+    private function formatUtilisateurForChoices(array $utilisateurs): array
+    {
+        $formattedUtilisateur = [];
+        foreach ($utilisateurs as $utilisateur) {
+            $formattedUtilisateur[$utilisateur->getNom()] = $utilisateur;
+        }
+        return $formattedUtilisateur;
     }
 }
