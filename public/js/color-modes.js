77c6bd943b5a6e466e1a177e117b0c3e1a2abced
hugodevweb <100534757+hugodevweb@users.noreply.github.com>
2023-12-20 22:15:21 +0100

    Update article
    r

--- public/js/color-modes.js
+++ public/js/color-modes.js
@@ -68,12 +68,12 @@
   
     // Chemin relatif par rapport au fichier HTML qui charge le script
     let imagePath;
-    let isParentPage = window.location.pathname === '/index.html' || window.location.pathname === '/';
-    if (isParentPage) {
-      imagePath = '../../../public/img/';
-    } else {
-      imagePath = './../../public/img/';
-    }
+    // let isParentPage = window.location.pathname === '/index.html' || window.location.pathname === '/';
+    // if (isParentPage) {
+    //   imagePath = '../../../public/img/';
+    // } else {
+      imagePath = '/img/';
+    //}
     if (theme === 'dark') {
       logoElement.src = imagePath + 'logoFN.png';
       logoElement.alt = 'Dark Logo';
