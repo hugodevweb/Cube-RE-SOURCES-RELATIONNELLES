d5fae0124390f5a73e52f1c61d26d52f9c052110
Mysterdoc17 <jeremie.baudon.17@gmail.com>
2024-06-18 14:59:50 +0200

    ajout de l'entité contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,42 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use PHPMailer\PHPMailer\PHPMailer;
+
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $subject = $_POST['subject'];
-            $message = $_POST['message'];
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
 
-            // Validation et envoi de l'email (par exemple)
-            if (!empty($name) && !empty($email) && !empty($message) && !empty($subject)) {
-                // Logique d'envoi d'email
-                $mail = new PHPMailer(true);
-                $mail->isSMTP();
-                $mail->Host = 'smtp.ethereal.email';
-                $mail->SMTPAuth = true;
-                $mail->Username = 'valentine.cartwright8@ethereal.email';
-                $mail->Password = 'VVTnKfyeXEujKr36Ks';
-                $mail->SMTPSecure = 'tls';
-                $mail->Port = 587;
-                $mail->setFrom('valentine.cartwright8@ethereal.email', 'Valentine Cartwright');
-                $mail->Subject = $subject;
-                $mail->addAddress($email, 'Nom Destinataire');
-                $mail->Body = $message;
-                $mail->send();
-            }
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Your message has been sent successfully!');
+
+            return $this->redirectToRoute('app_contact');
         }
         return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
