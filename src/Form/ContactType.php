aff345f52096535aa0f3e35aa92e45a09b6a9ed8
Mysterdoc17 <jeremie.baudon.17@gmail.com>
2024-06-18 16:49:34 +0200

    update pour le traitement de la page de contact en fonction de l'état de la connexion

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,23 +6,38 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        if (!$options['is_authenticated']) {
+            $builder
+                ->add('name', TextType::class, [
+                    'attr' => ['class' => 'form-control']
+                ])
+                ->add('email', EmailType::class, [
+                    'attr' => ['class' => 'form-control']
+                ]);
+        }
+
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('subject')
-            ->add('message')
-        ;
+            ->add('subject', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('message', TextareaType::class, [
+                'attr' => ['class' => 'form-control']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Contact::class,
+            'is_authenticated' => false,
         ]);
     }
 }
