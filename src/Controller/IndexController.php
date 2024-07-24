720119e817914a463ba739be173075bb226cf6b0
EmilieBgnr <bogneremilie@gmail.com>
2024-07-24 16:52:09 +0200

    feat(templates/css/controller): UPDATE - Pages du footer
    * Toutes les pages du footer ont été faites, desktop et responsive, css aussi, et le css du footer a aussi été MAJ.
    * Les routes ont été créées dans l'indexController (controller par défaut)

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -17,4 +17,34 @@ class IndexController extends AbstractController
             'carousel' => $indexRepository->carousel($entityManager),
         ]);
     }
+
+    #[Route('/a-propos', name: 'a-propos')]
+    public function aboutUs(): Response
+    {
+        return $this->render('footer_pages/about_us.html.twig');
+    }
+
+    #[Route('/mentions-legales', name: 'mentions')]
+    public function importContact(): Response
+    {
+        return $this->render('footer_pages/mentions.html.twig');
+    }
+
+    #[Route('/donnees-personnelles', name: 'donnees-perso')]
+    public function donneesPerso(): Response
+    {
+        return $this->render('footer_pages/donnees_perso.html.twig');
+    }
+
+    #[Route('/cookies', name: 'cookies')]
+    public function cookies(): Response
+    {
+        return $this->render('footer_pages/cookies.html.twig');
+    }
+
+    #[Route('/plan-du-site', name: 'sitemap')]
+    public function sitemap(): Response
+    {
+        return $this->render('footer_pages/sitemap.html.twig');
+    }
 }
